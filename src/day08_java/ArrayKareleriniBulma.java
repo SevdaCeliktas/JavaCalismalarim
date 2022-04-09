6f222f183adf143f6335c790f3f1c382bcbf0d67
Sevda Nazlı Çeliktaş <sevdaaa.nazliii@gmail.com>
2022-04-10 02:31:10 +0300

    sekizinci commit

---
+++ src/day08_java/ArrayKareleriniBulma.java
@@ -0,0 +1,22 @@
+package day08_java;
+
+import java.util.Arrays;
+
+public class ArrayKareleriniBulma {
+
+    public static void main(String[] args) {
+
+        //Verilen bi int dizisi icin elemanlarin karelerini bulunuz.
+        // Example: {2,6,4,5,8,9}
+        //Output: {4,36,16,25,64,81}
+
+                int arr[] = {2,6,4,5,8,9};
+                for (int i = 0; i < arr.length ; i++) {
+                    arr[i] *=  arr[i];
+                }
+                System.out.println("Elemanlarin karesi : " + Arrays.toString(arr));
+
+            }
+        }
+
+
